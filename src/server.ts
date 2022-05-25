e20c5aa674ab580523a3b1380b4223ca7fc46950
Allan Scala <49344838+AllanScala1991@users.noreply.github.com>
2022-05-25 20:41:52 -0300

    fix: create cors config

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,12 @@ import "dotenv/config"
 const app: Application = express()
 const port: number = parseInt(`${process.env.PORT}`) || 8000
 
-app.use(cors())
+var corsOptions = {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'] 
+}
+app.use(cors(corsOptions))
 app.use(body.json())
 app.use(body.urlencoded({extended: false}))
 
@@ -16,23 +21,6 @@ app.use(require("./routes/login"))
 app.use(require("./routes/board"))
 app.use(require("./routes/stage"))
 app.use(require("./routes/task"))
-
-//CORS
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-})
 
 app.listen(port, () => {
     return console.log("Server is running")
